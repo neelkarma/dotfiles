4043e987a371515ca8c861a02e0a588e158af54f
Neel Sharma <nsdj.sharma@gmail.com>
2025-10-09 15:48:33 +1100

    add more quickfix navigation binds

--- .config/nvim/ftplugin/qf.lua
+++ .config/nvim/ftplugin/qf.lua
@@ -2,3 +2,5 @@ local map = vim.keymap.set
 
 map("n", "j", "j<CR><C-w>p", { buffer = true })
 map("n", "k", "k<CR><C-w>p", { buffer = true })
+map("n", "gg", "gg<CR><C-w>p", { buffer = true })
+map("n", "G", "G<CR><C-w>p", { buffer = true })
